bb69fb04350135a0e501aed285b4c49c5001fcf3
streamich <streamich@gmail.com>
2020-10-05 13:57:57 +0200

    feat: 🎸 implement genProps v1

---
+++ src/util/genProps/__tests__/genProps.spec.ts
@@ -0,0 +1,245 @@
+import {genProps as v1} from '../v1';
+import {parseProps, parseVarInt} from '../../parse';
+import { PROPERTY } from '../../../enums';
+
+const generators = [v1];
+
+for (let i = 0; i < generators.length; i++) {
+  const genProps = generators[i];
+  describe(`genProps v${i + 1}`, () => {
+    test('a single one byte property', () => {
+      const buf = genProps({
+        [PROPERTY.PayloadFormatIndicator]: 0,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.PayloadFormatIndicator]: 0,
+      });
+    });
+
+    test('one byte properties', () => {
+      const buf = genProps({
+        [PROPERTY.PayloadFormatIndicator]: 1,
+        [PROPERTY.RequestProblemInformation]: 2,
+        [PROPERTY.RequestResponseInformation]: 3,
+        [PROPERTY.MaximumQoS]: 4,
+        [PROPERTY.RetainAvailable]: 5,
+        [PROPERTY.WildcardSubscriptionAvailable]: 6,
+        [PROPERTY.SubscriptionIdentifierAvailable]: 7,
+        [PROPERTY.SharedSubscriptionAvailable]: 8,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.PayloadFormatIndicator]: 1,
+        [PROPERTY.RequestProblemInformation]: 2,
+        [PROPERTY.RequestResponseInformation]: 3,
+        [PROPERTY.MaximumQoS]: 4,
+        [PROPERTY.RetainAvailable]: 5,
+        [PROPERTY.WildcardSubscriptionAvailable]: 6,
+        [PROPERTY.SubscriptionIdentifierAvailable]: 7,
+        [PROPERTY.SharedSubscriptionAvailable]: 8,
+      });
+    });
+
+    test('two byte properties', () => {
+      const buf = genProps({
+        [PROPERTY.ServerKeepAlive]: 301,
+        [PROPERTY.ReceiveMaximum]: 302,
+        [PROPERTY.TopicAliasMaximum]: 303,
+        [PROPERTY.TopicAlias]: 304,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.ServerKeepAlive]: 301,
+        [PROPERTY.ReceiveMaximum]: 302,
+        [PROPERTY.TopicAliasMaximum]: 303,
+        [PROPERTY.TopicAlias]: 304,
+      });
+    });
+
+    test('one and two byte property mix', () => {
+      const buf = genProps({
+        [PROPERTY.ServerKeepAlive]: 301,
+        [PROPERTY.SharedSubscriptionAvailable]: 8,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.ServerKeepAlive]: 301,
+        [PROPERTY.SharedSubscriptionAvailable]: 8,
+      });
+    });
+
+    test('a single 4 byte property', () => {
+      const buf = genProps({
+        [PROPERTY.MessageExpiryInterval]: 34 * 0xffffff,
+      });
+      expect(buf.length).toBe(6);
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.MessageExpiryInterval]: 34 * 0xffffff,
+      });
+    });
+
+    test('4 byte properties', () => {
+      const buf = genProps({
+        [PROPERTY.MessageExpiryInterval]: 1,
+        [PROPERTY.WillDelayInterval]: 323,
+        [PROPERTY.SessionExpiryInterval]: 323 * 255,
+        [PROPERTY.MaximumPacketSize]: 2_000_000_000,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.MessageExpiryInterval]: 1,
+        [PROPERTY.WillDelayInterval]: 323,
+        [PROPERTY.SessionExpiryInterval]: 323 * 255,
+        [PROPERTY.MaximumPacketSize]: 2_000_000_000,
+      });
+    });
+
+    test('SubscriptionIdentifier with small value', () => {
+      const buf = genProps({
+        [PROPERTY.SubscriptionIdentifier]: 1,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(buf.length).toBe(1 + 1 + 1);
+      expect(props).toEqual({
+        [PROPERTY.SubscriptionIdentifier]: 1,
+      });
+    });
+
+    test('SubscriptionIdentifier with medium value', () => {
+      const buf = genProps({
+        [PROPERTY.SubscriptionIdentifier]: 333,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.SubscriptionIdentifier]: 333,
+      });
+    });
+
+    test('SubscriptionIdentifier with large value', () => {
+      const buf = genProps({
+        [PROPERTY.SubscriptionIdentifier]: 2e6,
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.SubscriptionIdentifier]: 2e6,
+      });
+    });
+
+    test('a single binary field', () => {
+      const buf = genProps({
+        [PROPERTY.CorrelationData]: Buffer.from([0]),
+      });
+      expect(buf.length).toBe(1 + 1 + 2 + 1);
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.CorrelationData]: Buffer.from([0]),
+      });
+    });
+
+    test('two binary fields', () => {
+      const buf = genProps({
+        [PROPERTY.CorrelationData]: Buffer.from([0]),
+        [PROPERTY.AuthenticationData]: Buffer.from([1, 2, 3]),
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.CorrelationData]: Buffer.from([0]),
+        [PROPERTY.AuthenticationData]: Buffer.from([1, 2, 3]),
+      });
+    });
+
+    test('can encode ContentType', () => {
+      const buf = genProps({
+        [PROPERTY.ContentType]: 'application/json',
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.ContentType]: 'application/json',
+      });
+    });
+
+    test('can encode emojis', () => {
+      const buf = genProps({
+        [PROPERTY.ContentType]: '👍',
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.ContentType]: '👍',
+      });
+    });
+
+    test('many string properties', () => {
+      const buf = genProps({
+        [PROPERTY.ContentType]: '👍',
+        [PROPERTY.ResponseTopic]: '/topic/+/#',
+        [PROPERTY.AssignedClientIdentifier]: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+        [PROPERTY.AuthenticationMethod]: 'jwt',
+        [PROPERTY.ResponseInformation]: 'Not Found',
+        [PROPERTY.ServerReference]: 'Cloud Flare',
+        [PROPERTY.ReasonString]: 'Unknown',
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.ContentType]: '👍',
+        [PROPERTY.ResponseTopic]: '/topic/+/#',
+        [PROPERTY.AssignedClientIdentifier]: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+        [PROPERTY.AuthenticationMethod]: 'jwt',
+        [PROPERTY.ResponseInformation]: 'Not Found',
+        [PROPERTY.ServerReference]: 'Cloud Flare',
+        [PROPERTY.ReasonString]: 'Unknown',
+      });
+    });
+
+    test('can encode user property', () => {
+      const buf = genProps({
+        [PROPERTY.UserProperty]: [
+          ['X-Forward-Ip', '127.0.0.1'],
+        ],
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.UserProperty]: [
+          ['X-Forward-Ip', '127.0.0.1'],
+        ],
+      });
+    });
+
+    test('can have multiple user properties', () => {
+      const buf = genProps({
+        [PROPERTY.UserProperty]: [
+          ['1', 'a'],
+          ['2', 'b'],
+          ['3', 'c'],
+        ],
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.UserProperty]: [
+          ['1', 'a'],
+          ['2', 'b'],
+          ['3', 'c'],
+        ],
+      });
+    });
+
+    test('can set same property multiple times', () => {
+      const buf = genProps({
+        [PROPERTY.UserProperty]: [
+          ['key', 'value-1'],
+          ['key', 'value-2'],
+          ['key', 'value-3'],
+        ],
+      });
+      const props = parseProps(buf, 0)[0];
+      expect(props).toEqual({
+        [PROPERTY.UserProperty]: [
+          ['key', 'value-1'],
+          ['key', 'value-2'],
+          ['key', 'value-3'],
+        ],
+      });
+    });
+  });
+}
---
+++ src/util/genProps/v1.ts
@@ -0,0 +1,213 @@
+import { PROPERTY } from '../../enums';
+import {Properties} from '../../types';
+import {genVarInt} from '../genVarInt/v5';
+
+const identifiers = [
+    PROPERTY.PayloadFormatIndicator,
+    PROPERTY.MessageExpiryInterval,
+    PROPERTY.ContentType,
+    PROPERTY.ResponseTopic,
+    PROPERTY.CorrelationData,
+    PROPERTY.SubscriptionIdentifier,
+    PROPERTY.SessionExpiryInterval,
+    PROPERTY.AssignedClientIdentifier,
+    PROPERTY.ServerKeepAlive,
+    PROPERTY.AuthenticationMethod,
+    PROPERTY.AuthenticationData,
+    PROPERTY.RequestProblemInformation,
+    PROPERTY.WillDelayInterval,
+    PROPERTY.RequestResponseInformation,
+    PROPERTY.ResponseInformation,
+    PROPERTY.ServerReference,
+    PROPERTY.ReasonString,
+    PROPERTY.ReceiveMaximum,
+    PROPERTY.TopicAliasMaximum,
+    PROPERTY.TopicAlias,
+    PROPERTY.MaximumQoS,
+    PROPERTY.RetainAvailable,
+    PROPERTY.UserProperty,
+    PROPERTY.MaximumPacketSize,
+    PROPERTY.WildcardSubscriptionAvailable,
+    PROPERTY.SubscriptionIdentifierAvailable,
+    PROPERTY.SharedSubscriptionAvailable,  
+];
+
+export const genProps = (props: Properties): Buffer => {
+  const bytes: number[] = [];
+  const buffers: Buffer[] = [];
+  let buffersSize: number = 0;
+  let value: any;
+
+  // 1 byte properties
+  if (props[PROPERTY.PayloadFormatIndicator] !== undefined)
+    bytes.push(PROPERTY.PayloadFormatIndicator, props[PROPERTY.PayloadFormatIndicator]!);
+  if (props[PROPERTY.RequestProblemInformation] !== undefined)
+    bytes.push(PROPERTY.RequestProblemInformation, props[PROPERTY.RequestProblemInformation]!);
+  if (props[PROPERTY.RequestResponseInformation] !== undefined)
+    bytes.push(PROPERTY.RequestResponseInformation, props[PROPERTY.RequestResponseInformation]!);
+  if (props[PROPERTY.MaximumQoS] !== undefined)
+    bytes.push(PROPERTY.MaximumQoS, props[PROPERTY.MaximumQoS]!);
+  if (props[PROPERTY.RetainAvailable] !== undefined)
+    bytes.push(PROPERTY.RetainAvailable, props[PROPERTY.RetainAvailable]!);
+  if (props[PROPERTY.WildcardSubscriptionAvailable] !== undefined)
+    bytes.push(PROPERTY.WildcardSubscriptionAvailable, props[PROPERTY.WildcardSubscriptionAvailable]!);
+  if (props[PROPERTY.SubscriptionIdentifierAvailable] !== undefined)
+    bytes.push(PROPERTY.SubscriptionIdentifierAvailable, props[PROPERTY.SubscriptionIdentifierAvailable]!);
+  if (props[PROPERTY.SharedSubscriptionAvailable] !== undefined)
+    bytes.push(PROPERTY.SharedSubscriptionAvailable, props[PROPERTY.SharedSubscriptionAvailable]!);
+
+  // 2 byte properties
+  if (props[PROPERTY.ServerKeepAlive] !== undefined)
+    bytes.push(PROPERTY.ServerKeepAlive, (props[PROPERTY.ServerKeepAlive]! & 0xFF00) >> 8, props[PROPERTY.ServerKeepAlive]! & 0xFF);
+  if (props[PROPERTY.ReceiveMaximum] !== undefined)
+    bytes.push(PROPERTY.ReceiveMaximum, (props[PROPERTY.ReceiveMaximum]! & 0xFF00) >> 8, props[PROPERTY.ReceiveMaximum]! & 0xFF);
+  if (props[PROPERTY.TopicAliasMaximum] !== undefined)
+    bytes.push(PROPERTY.TopicAliasMaximum, (props[PROPERTY.TopicAliasMaximum]! & 0xFF00) >> 8, props[PROPERTY.TopicAliasMaximum]! & 0xFF);
+  if (props[PROPERTY.TopicAlias] !== undefined)
+    bytes.push(PROPERTY.TopicAlias, (props[PROPERTY.TopicAlias]! & 0xFF00) >> 8, props[PROPERTY.TopicAlias]! & 0xFF);
+
+  // 4 byte properties
+  if (props[PROPERTY.MessageExpiryInterval] !== undefined)
+    bytes.push(
+      PROPERTY.MessageExpiryInterval,
+      (props[PROPERTY.MessageExpiryInterval]! & 0xFF000000) >> 24,
+      (props[PROPERTY.MessageExpiryInterval]! & 0xFF0000) >> 16,
+      (props[PROPERTY.MessageExpiryInterval]! & 0xFF00) >> 8,
+      props[PROPERTY.MessageExpiryInterval]! & 0xFF
+    );
+  if (props[PROPERTY.WillDelayInterval] !== undefined)
+    bytes.push(
+      PROPERTY.WillDelayInterval,
+      (props[PROPERTY.WillDelayInterval]! & 0xFF000000) >> 24,
+      (props[PROPERTY.WillDelayInterval]! & 0xFF0000) >> 16,
+      (props[PROPERTY.WillDelayInterval]! & 0xFF00) >> 8,
+      props[PROPERTY.WillDelayInterval]! & 0xFF
+    );
+  if (props[PROPERTY.SessionExpiryInterval] !== undefined)
+    bytes.push(
+      PROPERTY.SessionExpiryInterval,
+      (props[PROPERTY.SessionExpiryInterval]! & 0xFF000000) >> 24,
+      (props[PROPERTY.SessionExpiryInterval]! & 0xFF0000) >> 16,
+      (props[PROPERTY.SessionExpiryInterval]! & 0xFF00) >> 8,
+      props[PROPERTY.SessionExpiryInterval]! & 0xFF
+    );
+  if (props[PROPERTY.MaximumPacketSize] !== undefined)
+    bytes.push(
+      PROPERTY.MaximumPacketSize,
+      (props[PROPERTY.MaximumPacketSize]! & 0xFF000000) >> 24,
+      (props[PROPERTY.MaximumPacketSize]! & 0xFF0000) >> 16,
+      (props[PROPERTY.MaximumPacketSize]! & 0xFF00) >> 8,
+      props[PROPERTY.MaximumPacketSize]! & 0xFF
+    );
+
+  // Variable length integers
+  value = props[PROPERTY.SubscriptionIdentifier];
+  if (value !== undefined) {
+    const int = genVarInt(value);
+    buffersSize += 1 + int.length;
+    buffers.push(Buffer.from([PROPERTY.SubscriptionIdentifier]), int);
+  }
+
+  // Binary data
+  value = props[PROPERTY.CorrelationData];
+  if (value) {
+    const buf = Buffer.allocUnsafe(3);
+    buf.writeUInt8(PROPERTY.CorrelationData);
+    buf.writeUInt16BE(value.length, 1);
+    buffers.push(buf, value);
+    buffersSize += 3 + value.length;
+  }
+  value = props[PROPERTY.AuthenticationData];
+  if (value) {
+    const buf = Buffer.allocUnsafe(3);
+    buf.writeUInt8(PROPERTY.AuthenticationData);
+    buf.writeUInt16BE(value.length, 1);
+    buffers.push(buf, value);
+    buffersSize += 3 + buf.length;
+  }
+
+  value = props[PROPERTY.ContentType];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.ContentType);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.ResponseTopic];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.ResponseTopic);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.AssignedClientIdentifier];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.AssignedClientIdentifier);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.AuthenticationMethod];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.AuthenticationMethod);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.ResponseInformation];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.ResponseInformation);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.ServerReference];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.ServerReference);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+  value = props[PROPERTY.ReasonString];
+  if (value !== undefined) {
+    const buf = Buffer.allocUnsafe(3);
+    const buf2 = Buffer.from(value, 'utf8');
+    buf.writeUInt8(PROPERTY.ReasonString);
+    buf.writeUInt16BE(buf2.length, 1);
+    buffers.push(buf, buf2);
+    buffersSize += 3 + buf2.length;
+  }
+
+  value = props[PROPERTY.UserProperty];
+  if (value) {
+    for (const [k, v] of value) {
+      const buf1 = Buffer.allocUnsafe(3);
+      const buf2 = Buffer.from(k, 'utf8');
+      const buf3 = Buffer.allocUnsafe(2);
+      const buf4 = Buffer.from(v, 'utf8');
+      buf1.writeUInt8(PROPERTY.UserProperty);
+      buf1.writeUInt16BE(buf2.length, 1);
+      buf3.writeUInt16BE(buf4.length, 0);
+      buffers.push(buf1, buf2, buf3, buf4);
+      buffersSize += 3 + buf2.length + 2 + buf4.length;
+    }
+  }
+
+  return Buffer.concat([
+    genVarInt(bytes.length + buffersSize),
+    Buffer.from(bytes),
+    ...buffers,
+  ]);
+};
