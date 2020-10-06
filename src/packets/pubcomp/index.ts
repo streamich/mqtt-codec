77816a6acdbf766e428c8d9037b5da201e80eeeb
streamich <streamich@gmail.com>
2020-10-06 21:37:37 +0200

    feat: 🎸 add encoding for pubcomp packets

--- src/packets/pubcomp/index.ts
+++ src/packets/pubcomp/index.ts
@@ -1,6 +1,8 @@
-import {Packet, PacketHeaderData} from '../packet';
-import {BufferLike, Properties} from '../types';
-import {parseProps} from '../util/parseProps';
+import {PACKET_TYPE} from '../../enums';
+import {Packet, PacketHeaderData} from '../../packet';
+import {BufferLike, Properties} from '../../types';
+import {parseProps} from '../../util/parseProps';
+import {encodePuback} from '../puback/encodePuback';
 
 export interface PacketPubcompData extends PacketHeaderData {
   /** Packet Identifier. */
@@ -12,6 +14,15 @@ export interface PacketPubcompData extends PacketHeaderData {
 }
 
 export class PacketPubcomp extends Packet implements PacketPubcompData {
+  /**
+   * @param i Packet Identifier
+   * @param c Reason Code
+   * @param p Properties
+   */
+  static create(i: number, c: number, p: Properties) {
+    return new PacketPubcomp(PACKET_TYPE.PUBCOMP << 4, 0, i, c, p);
+  }
+
   constructor(
     b: number,
     l: number,
@@ -20,6 +31,10 @@ export class PacketPubcomp extends Packet implements PacketPubcompData {
     public p: Properties,
   ) {
     super(b, l);
+  }
+
+  public toBuffer(version: number) {
+    return encodePuback(this, version);
   }
 }
 
