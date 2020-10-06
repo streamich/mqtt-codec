332cde26bc055b261324a3713be56d76988b41d3
streamich <streamich@gmail.com>
2020-10-06 18:44:18 +0200

    refactor: 💡 create a folder for parseProps(

---
+++ benchmarks/parseProps.js
@@ -0,0 +1,49 @@
+const Benchmark = require('benchmark');
+const {genProps} =  require('../es6/util/genProps');
+
+const versions = {
+  v1: require('../es6/util/parseProps/v1').parseProps,
+  // v2: require('../es6/util/parseProps/v2').parseProps,
+};
+
+const suite = new Benchmark.Suite;
+
+const props = {
+  '2': 120, // MessageExpiryInterval
+  '25': 1, // RequestResponseInformation
+  '35': 123, // TopicAlias
+  '11': 245555, // SubscriptionIdentifier
+  '33': 20000, // ReceiveMaximum
+  '37': 100, // RetainAvailable
+  '8': 'europe/germany/munich/sensors/temperature/room-32/termostats/#', // ResponseTopic
+  '9': Buffer.from('xcxcxcxc-xcxc-xcxc-xcxc-xcxcxcxcxcxc', 'utf8'), // CorrelationData
+  '38': [ // UserProperty
+    [ 'Authorization', '=LAIJGORlkJMA9833LK2-LFDl:ADF83-03239234LKFJLDSLSD' ],
+    [ 'Ip-Address', '0.0.0.0' ],
+    [ 'Cache', 'no-cache' ],
+  ],
+};
+const buf = genProps(props);
+
+const run = (parseProps) => {
+  const p = parseProps(buf, 0);
+  // console.log(p);
+};
+
+// run(versions.v1);
+
+for (const name in versions) {
+  suite
+    .add(`parseProps (${name})`, function() {
+      run(versions[name]);
+    })
+}
+
+suite
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run();
---
+++ src/util/parseProps/v1.ts
@@ -0,0 +1,82 @@
+import {ERROR, PROPERTY} from '../../enums';
+import {BufferLike, Properties} from '../../types';
+import {parseBinary, parseVarInt} from '../parse';
+
+export const parseProps = (data: BufferLike, offset: number): [props: Properties, size: number] => {
+  // TODO: inline length parsing
+  const [int, size] = parseVarInt(data, offset);
+  offset += size;
+  const end = offset + int;
+  const props: Properties = {};
+  while (offset < end) {
+    const byte = data.readUInt8(offset++);
+    switch (byte) {
+      case PROPERTY.PayloadFormatIndicator:
+      case PROPERTY.RequestProblemInformation:
+      case PROPERTY.RequestResponseInformation:
+      case PROPERTY.MaximumQoS:
+      case PROPERTY.RetainAvailable:
+      case PROPERTY.WildcardSubscriptionAvailable:
+      case PROPERTY.SubscriptionIdentifierAvailable:
+      case PROPERTY.SharedSubscriptionAvailable: {
+        props[byte] = data.readUInt8(offset);
+        offset += 1;
+        break;
+      }
+      case PROPERTY.ServerKeepAlive:
+      case PROPERTY.ReceiveMaximum:
+      case PROPERTY.TopicAliasMaximum:
+      case PROPERTY.TopicAlias: {
+        props[byte] = data.readUInt16BE(offset);
+        offset += 2;
+        break;
+      }
+      case PROPERTY.MessageExpiryInterval:
+      case PROPERTY.WillDelayInterval:
+      case PROPERTY.SessionExpiryInterval:
+      case PROPERTY.MaximumPacketSize: {
+        props[byte] = data.readUInt32BE(offset);
+        offset += 4;
+        break;
+      }
+      case PROPERTY.SubscriptionIdentifier: {
+        const [value, size] = parseVarInt(data, offset);
+        props[byte] = value;
+        offset += size;
+        break;
+      }
+      case PROPERTY.CorrelationData:
+      case PROPERTY.AuthenticationData: {
+        // TODO: inline "parseBinary" and use switch fall-through.
+        const {byteLength} = (props[byte] = parseBinary(data, offset));
+        offset += 2 + byteLength;
+        break;
+      }
+      case PROPERTY.ContentType:
+      case PROPERTY.ResponseTopic:
+      case PROPERTY.AssignedClientIdentifier:
+      case PROPERTY.AuthenticationMethod:
+      case PROPERTY.ResponseInformation:
+      case PROPERTY.ServerReference:
+      case PROPERTY.ReasonString: {
+        const binary = parseBinary(data, offset);
+        props[byte] = binary.toString('utf8');
+        offset += 2 + binary.byteLength;
+        break;
+      }
+      case PROPERTY.UserProperty: {
+        if (!props[PROPERTY.UserProperty]) props[PROPERTY.UserProperty] = [];
+        const key = parseBinary(data, offset);
+        offset += 2 + key.byteLength;
+        const value = parseBinary(data, offset);
+        offset += 2 + value.byteLength;
+        props[PROPERTY.UserProperty]!.push([key.toString('utf8'), value.toString('utf8')]);
+        break;
+      }
+      default: {
+        throw ERROR.MALFORMED_PACKET;
+      }
+    }
+  }
+  return [props, int + size];
+};
